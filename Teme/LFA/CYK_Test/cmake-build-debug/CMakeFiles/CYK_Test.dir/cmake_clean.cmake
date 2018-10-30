a600ebe2573f8dd5e57e72cad326936b8726a56a
Corina Tudorache <Corina-Stefania@users.noreply.github.com>
2018-10-30 15:19:17 +0200

    Add files via upload

---
+++ Teme/LFA/CYK_Test/cmake-build-debug/CMakeFiles/CYK_Test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CYK_Test.dir/main.cpp.obj"
+  "CYK_Test.pdb"
+  "CYK_Test.exe"
+  "CYK_Test.exe.manifest"
+  "libCYK_Test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CYK_Test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
